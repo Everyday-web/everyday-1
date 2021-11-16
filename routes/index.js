174ad24f83ec574411ac0d9d014c85991ee73fc2
ProyWanat <proy3258@gmail.com>
2021-11-17 02:45:12 +0700

    [update] FrontEnd_Customer

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,43 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  res.render('admin');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('cart');
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('products');
+});
+
+router.get('/product-detail', function(req, res, next) {
+  res.render('product-detail');
+});
+
+router.get('/status', function(req, res, next) {
+  res.render('status');
+});
+
+router.get('/order-detail', function(req, res, next) {
+  res.render('order-detail');
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout');
+});
+
+
 router.get('/login', function(req, res, next){
   res.render('login')
 })
