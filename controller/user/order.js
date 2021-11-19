d316b6b7d52fc953dcf999413593add86e6ec634
thoraninchaiya <thoraninchaiya@gmail.com>
2021-11-20 03:59:53 +0700

    [u] order, index

--- controller/user/order.js
+++ controller/user/order.js
@@ -1,10 +1,15 @@
 var db = require('../../db/db')
 
-function getorders(req, res) {
+function getorders(req, res, next) {
     console.log(req.userData)
-    db.query(`SELECT * FROM order`, (selerr, selresults) =>{
+    db.query(`SELECT * FROM orders WHERE user_id = ${req.userData.userid}`, (selerr, selresults) =>{
         if(selerr) throw selerr
-        console.log(selresults);
+        // console.log(selresults);
+        if(selresults === undefined || selresults.length == 0){
+            return 
+        }
+        req.orderlist = selresults
+        return next()
     })
 }
 
